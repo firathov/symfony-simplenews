6be5bd3fe879b32f877dc7d1bd72583ec187604c
firathov <firat.hov@gmail.com>
2022-04-08 21:54:59 +0400

    CommentCotroller's API is ready

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -7,6 +7,9 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class CommentController extends AbstractController
 {
@@ -26,5 +29,76 @@ class CommentController extends AbstractController
         $entityManager->persist($comment);
         $entityManager->flush();
         return new Response('Saved new comment with id ' . $comment->getId());
+    }
+
+    /**
+     * Action to show all comments from DB
+     *
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
+    public function showAll(ManagerRegistry $doctrine):Response
+    {
+        $encoder = [new JsonEncode()];
+        $normalizer = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizer, $encoder);
+        $repository = $doctrine->getRepository(Comment::class);
+        $result = $repository->findAll();
+        $jsonContent = $serializer->serialize($result, 'json');
+        return new Response($jsonContent);
+    }
+
+    /**
+     * Action to update a comment by I'd (Method: POST)
+     *
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * @param Request $request
+     * @return Response
+     */
+    public function update(ManagerRegistry $doctrine, int $id, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No post found for id ' . $id
+            );
+        }
+
+        $data = $request->get('content');
+
+        if (!isset($data)) {
+            return new Response("Invalid Input");
+        }
+        $comment->setContent($data);
+        $entityManager->persist($comment);
+        $entityManager->flush();
+
+        return new Response('Comment with id ' . $comment->getId() . ' updated');
+    }
+
+    /**
+     * Action to delete a comment by id (Method: DELETE)
+     *
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * @return Response
+     */
+    public function remove(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        return new Response('Comment has been deleted');
     }
 }
